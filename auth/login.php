d36e8806e3cf51dfc625b16e2e7d63e8e7b4a494
Shaaams <60063149+Shaaams@users.noreply.github.com>
2020-04-06 17:50:51 +0300

    update login.php

--- auth/login.php
+++ auth/login.php
@@ -9,4 +9,16 @@ function spf_login_user( $requst ){
     //check value from field is exist or no
     // ( if no response return, so return message instead of value field )
     spf_validate_required_fields($params , ['email', 'password']);
+    
+    $result =wp_authenticate_username_password(null , $params['email'], $params['password']);
+
+    if (is_wp_error($result)){
+        wp_send_json([
+            'message' => 'User Credential dos\'t match our record',
+        ],403);
+    }
+    wp_send_json([
+        //'api_token' => $result->api_token,
+        'result'    => $result->ID
+    ],200);
 }
